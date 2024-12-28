10f2196354d8b6c9284923f93c635cd35f84b755
Mark Harfouche <mark.harfouche@gmail.com>
2024-12-28 13:20:01 -0500

    Update install scripts

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,12 +1,15 @@
 @echo on
 
-md build
-pushd build
-cmake -G "Ninja" ^
-      -DBUILD_SHARED_LIBS=ON ^
-      -DCMAKE_INSTALL_PREFIX="%LIBRARY_PREFIX%" ^
-      -DCMAKE_PREFIX_PATH="%LIBRARY_PREFIX" ^
-      ..
+REM cmake install targets removed
+REM https://github.com/dcleblanc/SafeInt/issues/67
+mkdir %LIBRARY_PREFIX%\include
 if errorlevel 1 exit 1
-cmake --build . --target install --config Release
+
+REM As of July, 2022, there is also a C library [...]
+REM It consists of two files, safe_math.h, and safe_math_impl.h.
+copy SafeInt.hpp %LIBRARY_PREFIX%\include\.
+if errorlevel 1 exit 1
+copy safe_math.h %LIBRARY_PREFIX%\include\.
+if errorlevel 1 exit 1
+copy safe_math_impl.h %LIBRARY_PREFIX%\include\.
 if errorlevel 1 exit 1
